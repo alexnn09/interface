6444adea5e53f4694024ad9f4d9313a205906eef
Alexander Dorogin <alexnn09@mail.ru>
2018-06-15 15:12:45 +0300

    update : add db

--- logger.php
+++ logger.php
@@ -1,39 +1,96 @@
 <?php
 
 namespace log;
-require "inter.php";
+
 use Maknz\Slack\Client;
 require_once 'vendor/autoload.php';
+require "inter.php";
 
+class consoleAdapter implements IAdapter
+{
+    private $options;
+
+    public function configure(array $options=null)
+    {
+        $this->options=$options;
+    }
+
+    public function write(string $message)
+    {
+        echo $message;
+    }
+}
+
+class slackAdapter implements IAdapter
+{
+    private $options;
+
+    public function configure(array $options=null)
+    {
+        $this->options=$options;
+    }
+
+    public function write(string $message)
+    {   $options=$this->options;
+        $client= new Client($options['hook']);
+        $msg=$client->createMessage();
+        $msg->to($options['cname'])->send($message);
+    }
+}
+
+class dbAdapter implements IAdapter
+{
+    private $options;
+
+    public function configure(array $options=null)
+    {
+        $this->options=$options;
+    }
+
+    public function write(string $message)
+    {
+        $options=$this->options;
+        $connectdb=mysqli_connect($options['host'],$options['user'],$options['password'],'logs');
+        $query='insert into logs.logss(time, log) VALUES ('.time().',"'.$message.'")';
+        mysqli_query($connectdb, $query);
+    }
+}
+
+class fileAdapter implements IAdapter
+{
+    private $options;
+
+    public function configure(array $options=null)
+    {
+        $this->options=$options;
+    }
+
+    public function write(string $message)
+    {
+        $options=$this->options;
+        $newmsg=file_get_contents($options['filename']);
+        $newmsg .= $message.PHP_EOL;
+        file_put_contents($options['filename'], $newmsg);
+    }
+}
 
 class logger implements ILogger
 {
+    private $adapter;
 
-    public function log(string $message, string $filename, string $cname='#dev'){
-        {   if($filename=='console') {
-            echo "log :".$message;
-        } elseif ($filename=='slack')
-        {
-            $url="https://hooks.slack.com/services/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";
-            $client= new Client($url);
-            $msg=$client->createMessage();
-            $msg->to($cname)->send($message);
+    public function __construct(IAdapter $adapter)
+    {
+        $this->adapter=$adapter;
 
-
-        }
-        elseif ($filename=='db'){
-            $connectdb=mysqli_connect('127.0.0.1','developer','developer','logs');
-
-            $query='insert into logs.logss(time, log) VALUES ('.time().',"'.$message.'")';
-            mysqli_query($connectdb, $query);
-
-
-        }
-        else {
-            $newmsg=file_get_contents($filename);
-            $newmsg .= $message.PHP_EOL;
-            file_put_contents($filename, $newmsg);
-        }
-        }
     }
+    public function logger(string $message)
+    {
+        $this->adapter->write($message);
+    }
+
 }
+
+$adapter = new fileAdapter();
+$adapter->configure(array('filename' => 'loggie.txt'));
+$logger=new logger($adapter);
+$logger->logger('test message');
