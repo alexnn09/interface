79836d44c1ba3b7b69e7923a716b0d978f86f975
Alexander Dorogin <alexnn09@mail.ru>
2018-06-18 17:23:03 +0300

    update

--- bin/script4.php
+++ bin/script4.php
@@ -6,6 +6,6 @@ require '../vendor/autoload.php';
 
 
 $adapter = new SlackLoggerAdapter();
-$adapter->configure(array('hook' => 'https://hooks.slack.com/services/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', 'cname' => '#dev'));
+$adapter->configure(array('hook' => 'https://hooks.slack.com/services/T18MXJD70/B292Y9DE3/6rY5tfJ5JRjXDqB5BUcxP3', 'cname' => '#dev'));
 $logger = new Logger($adapter);
 $logger->logger('one');
